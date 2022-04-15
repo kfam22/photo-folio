3ada3891e6b475958bf3919bc1d45f9b50b8e29f
KFamurewa <kayla.famurewa@gmail.com>
2022-04-15 14:21:59 -0400

    wip- sliding image carousel implemented

---
+++ src/images.js
@@ -0,0 +1,10 @@
+import image1 from './assets/1photo.jpg';
+import image2 from './assets/2photo.jpg';
+import image3 from './assets/3photo.jpg';
+import image4 from './assets/4photo.jpg';
+import image5 from './assets/5photo.jpg';
+import image6 from './assets/6photo.jpg';
+
+let images = [image1, image2, image3, image4, image5, image6];
+
+export default images;
