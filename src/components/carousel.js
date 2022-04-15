30167f6ee91543adf028bbf205698a70ce48344a
KFamurewa <kayla.famurewa@gmail.com>
2022-04-15 15:07:31 -0400

    wip-carousel component

---
+++ src/components/carousel.js
@@ -0,0 +1,30 @@
+import {motion} from 'framer-motion';
+import {useRef, useEffect, useState} from 'react';
+import images from '../images';
+
+function Carousel() {
+
+    const [width, setWidth] = useState(0);
+    const carousel = useRef();
+  
+    useEffect(() => {
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }, []);
+    
+    return (
+       <motion.div ref={carousel} className='carousel' whileTap={{cursor: 'grabbing'}}>
+       <motion.div drag='x' dragConstraints={{right:0, left: -width}} className='inner-carousel'>
+          {images.map(image => {
+            return(
+              <motion.div className='item' key={image}>
+                <img src={image} alt=''/>
+              </motion.div>
+            );
+          })}
+       </motion.div>
+     </motion.div>  
+    )
+   
+}
+
+export default Carousel;
